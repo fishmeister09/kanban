394df1a3ee2a833565a66ba46573f5c1ab63f147
Arjun <arjunsethi33@gmail.com>
2023-05-01 10:25:18 +0530

    changed file name

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect, useContext } from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import { useNavigate } from 'react-router-dom';
-import plus from '../assets/images/plus.png';
 import DataContext from '../context/DataContext';
 import NewButton from './newButton';
-import style from './status.module.css';
+import style from './board.module.css';
 
-const Status = ({ statusIndex, status }) => {
+const Board = ({ statusIndex, status }) => {
   const { data, setData } = useContext(DataContext);
   const [title, setTitle] = useState(status.title);
   const navigate = useNavigate();
@@ -25,6 +24,12 @@ const Status = ({ statusIndex, status }) => {
     setData(tmpData);
     localStorage.setItem('data', JSON.stringify(tmpData));
   };
+  const deleteStatus = () => {
+    const tmpData = [...data];
+    tmpData.splice(statusIndex, 1);
+    setData(tmpData);
+    localStorage.setItem('data', JSON.stringify(tmpData));
+  };
 
   useEffect(() => {
     const newData = [...data];
@@ -37,13 +42,26 @@ const Status = ({ statusIndex, status }) => {
     <div className={style.status}>
       <div className={style.header}>
         <div className={style.title}>
-          <div>
-            <input value={title} onChange={(e) => handleChange(e)} />
-          </div>
+          <input value={title} onChange={(e) => handleChange(e)} />
           <span>{status.tasks.length}</span>
         </div>
-        <div className={style.buttons}>
-          <img src={plus} alt="plus" onClick={addStatus} />
+        <div>
+          <button className={style.button} onClick={deleteStatus}>
+            <svg viewBox="0 0 24 24" height={'20px'}>
+              <path
+                d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM9 11V17H11V11H9ZM13 11V17H15V11H13ZM9 4V6H15V4H9Z"
+                fill="rgba(155,155,155,1)"
+              ></path>
+            </svg>
+          </button>
+          <button className={style.button} onClick={addStatus}>
+            <svg viewBox="0 0 24 24" height={'20px'}>
+              <path
+                d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"
+                fill="rgba(152,152,152,1)"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
       <Droppable droppableId={status.id}>
@@ -86,4 +104,4 @@ const Status = ({ statusIndex, status }) => {
   );
 };
 
-export default Status;
+export default Board;
